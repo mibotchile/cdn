42539b4927e8d643bc58e93b31304b2fe152f624
vector-rc <fernando.rdcrz@gmail.com>
2023-12-14 10:06:04 -0500

    add new configs

--- olimpo/flow/gpt_test/src/api/sendMessage.js
+++ olimpo/flow/gpt_test/src/api/sendMessage.js
@@ -1,19 +1,28 @@
 import { appConfig } from "../app-state/config";
 
-export const getPrediction = async (data) =>
-  await fetch(`${appConfig.gpt_url}${appConfig.projectPath}chat`, {
-    headers: {
-      "Content-Type": "application/json",
-    },
-    method: "POST",
-    body: JSON.stringify(data),
-  }).then((res) => res.json());
+export const getPrediction = async (data) => {
+	const botUrl = `${appConfig.ssl ? "https" : "http"}://${appConfig.botHost}`;
 
-export const sendMessage = async (data) =>
-  await fetch(`${appConfig.gpt_url}${appConfig.projectPath}messages/send`, {
-    headers: {
-      "Content-Type": "application/json",
-    },
-    method: "POST",
-    body: JSON.stringify(data),
-  }).then((res) => res.json());
+	const res = await fetch(`${botUrl}${appConfig.projectPath}chat`, {
+		headers: {
+			"Content-Type": "application/json",
+		},
+		method: "POST",
+		body: JSON.stringify(data),
+	});
+
+	return res.json();
+};
+
+export const sendMessage = async (data) =>{
+	const botUrl = `${appConfig.ssl ? "https" : "http"}://${appConfig.botHost}`;
+
+	const res=await fetch(`${botUrl}/messages/send`, {
+		headers: {
+			"Content-Type": "application/json",
+		},
+		method: "POST",
+		body: JSON.stringify(data),
+	})
+  return res.json()
+}
